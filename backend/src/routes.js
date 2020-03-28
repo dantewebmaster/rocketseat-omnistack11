d7349eee0d3519116f03fa1fc0b774bac4d70325
Dante J. Anjos <dante.anjos@MBP-de-Dante.home>
2020-03-28 20:57:39 -0300

    Added Validators

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,20 +1,67 @@
 const express = require('express');
 const routes = express.Router();
 
+// Controllers
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
-routes.post('/sessions', SessionController.create);
+// Validators
+const IncidentValidator = require('./validators/IncidentValidator');
+const OngValidator = require('./validators/OngValidator');
+const ProfileValidator = require('./validators/ProfileValidator');
 
-routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+/**
+ * Sessions
+ */
+routes.post(
+  '/sessions',
+  SessionController.create,
+);
 
-routes.get('/profile', ProfileController.index);
+/**
+ * Ongs
+ */
+routes.get(
+  '/ongs',
+  OngController.index,
+);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.post(
+  '/ongs',
+  OngValidator.create(),
+  OngController.create,
+);
+
+/**
+ * Profile
+ */
+routes.get(
+  '/profile',
+  ProfileValidator.index(),
+  ProfileController.index,
+);
+
+/**
+ * Incidents
+ */
+routes.get(
+  '/incidents',
+  IncidentValidator.index(),
+  IncidentController.index,
+);
+
+routes.post(
+  '/incidents',
+  IncidentValidator.create(),
+  IncidentController.create,
+);
+
+routes.delete(
+  '/incidents/:id',
+  IncidentValidator.delete(),
+  IncidentController.delete,
+);
 
 module.exports = routes;
